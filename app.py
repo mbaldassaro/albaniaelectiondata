0c7659f85e77d608834d53678f43574ee7191f87
Michael <mbaldassaro@gmail.com>
2017-04-12 13:05:39 -0400

    init commit
    flask + bootstrap install

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+manager = Manager(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    manager.run()
+
